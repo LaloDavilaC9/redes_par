bb52f3dc4c0f54ebd118f464144c442a5338fd36
Cynthia Teran <al226582@edu.uaa.mx>
2022-12-04 15:41:29 -0600

    Guard actualizado

--- sistema_tutorias/src/app/permisos/permiso-alumno.guard.ts
+++ sistema_tutorias/src/app/permisos/permiso-alumno.guard.ts
@@ -13,7 +13,7 @@ export class PermisoAlumnoGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if (this.servicioAut.sesionActual !== 1) {
+      if (this.servicioAut.sesionActual == 0) {
         this.router.navigate(['inicio-sesion']);
       }
       return true;
