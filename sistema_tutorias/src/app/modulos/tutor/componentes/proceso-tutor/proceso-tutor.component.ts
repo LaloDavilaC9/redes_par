002e97260be1872c0a78f8c5b36d22929e0cb6b0
lalodc9 <al269314@edu.uaa.mx>
2022-12-04 23:34:48 -0600

    Diseños nuevos nuevos

--- sistema_tutorias/src/app/modulos/tutor/componentes/proceso-tutor/proceso-tutor.component.ts
+++ sistema_tutorias/src/app/modulos/tutor/componentes/proceso-tutor/proceso-tutor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { solicitud } from 'src/app/modelos/solicitud.model';
 
 @Component({
   selector: 'app-proceso-tutor',
@@ -6,10 +7,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./proceso-tutor.component.css']
 })
 export class ProcesoTutorComponent implements OnInit {
+  solicitudes : solicitud[] = []
 
   constructor() { }
 
   ngOnInit(): void {
+    const solicitud1 : solicitud ={
+      ID : 5,
+      alumnoAsesorado : {
+        ID : 269314,
+        nombre: "LALO",
+        apellidoPaterno : "DAVILA",
+        apellidoMaterno : "CAMPOS",
+        semestre : 3,
+        telefono : "4499205022",
+        correo : "eduardo.davilac9@gmail.com",
+        clave : "1234",
+        imagen : ""
+      },
+      tutorAsesorias : {
+        ID : 247101,
+        alumnoAsesorias :{
+          ID : 247101,
+          nombre: "Cynthia Maritza",
+          apellidoPaterno : "Terán",
+          apellidoMaterno : "Carranza",
+          semestre : 7,
+          telefono : "4499205022",
+          correo : "cynthia@gmail.com",
+          clave : "1234",
+          imagen : ""
+        },
+        materiasAsesorias: [
+          {
+            ID : 1,
+            nombre : "Estructuras de datos",
+            semestre : 3
+          }
+        ]
+      },
+      fechaPeticion : "25/01/2022",
+      urgencia : false,
+      materiaAsociada : {
+        ID : 1,
+        nombre : "Estructuras de datos",
+        semestre : 3
+      },
+      tema : "Ciclos",
+      descripcion: "No le entiendo a mi profe",
+      fechaAsesoria : "",
+      sitio : "",
+      modalidad : "",
+      tutoresNoDisponibles : []
+    }
+
+    this.solicitudes.push(solicitud1);
+    this.solicitudes.push(solicitud1);
+    this.solicitudes.push(solicitud1);
+    
+
   }
 
 }
