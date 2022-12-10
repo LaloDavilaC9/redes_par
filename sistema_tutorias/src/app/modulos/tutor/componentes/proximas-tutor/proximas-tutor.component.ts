5a5be7cb870e78598bcf8df9ad2a6b6adb11f6e2
lalodc9 <al269314@edu.uaa.mx>
2022-12-10 07:13:27 -0600

    Proximas tutor

--- sistema_tutorias/src/app/modulos/tutor/componentes/proximas-tutor/proximas-tutor.component.ts
+++ sistema_tutorias/src/app/modulos/tutor/componentes/proximas-tutor/proximas-tutor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { solicitud } from 'src/app/modelos/solicitud.model';
+import { Tutor } from 'src/app/modelos/tutor.model';
 
 @Component({
   selector: 'app-proximas-tutor',
@@ -9,25 +10,55 @@ import { solicitud } from 'src/app/modelos/solicitud.model';
 export class ProximasTutorComponent implements OnInit {
 
   solicitudes : solicitud[] = []
-
-  constructor() { }
+  tutorActual!: Tutor;
+  solicitudDetalle! : solicitud;
+  totalAsesorias : Number = 0;
+  constructor() { 
+    this.tutorActual = {
+      ID : 2,
+      alumnoAsesorias: {
+        ID : 247101,
+          nombre: "Cynthia Maritza",
+          apellidoPaterno : "Terán",
+          apellidoMaterno : "Carranza",
+          semestre : 7,
+          telefono : "4499205022",
+          correo : "cynthia@gmail.com",
+          clave : "1234",
+          imagen : ""
+      },
+      materiasAsesorias:[
+        {
+          ID : 1,
+          nombre: "Estructuras de Datos",
+          semestre: 3
+        },
+        {
+          ID : 2,
+          nombre: "Álgebra Lineal",
+          semestre: 2
+        },
+      ]
+    };
+  }
 
   ngOnInit(): void {
     const solicitud1 : solicitud ={
       ID : 5,
       alumnoAsesorado : {
         ID : 269314,
-        nombre: "LALO",
-        apellidoPaterno : "DAVILA",
-        apellidoMaterno : "CAMPOS",
-        semestre : 3,
+        nombre: "ALBERTO",
+        apellidoPaterno : "SÁNCHEZ",
+        apellidoMaterno : "RODRÍGUEZ",
+        semestre : 4,
         telefono : "4499205022",
-        correo : "eduardo.davilac9@gmail.com",
+        correo : "alberto.snchez966@gmail.com",
         clave : "1234",
         imagen : ""
       },
+
       tutorAsesorias : {
-        ID : 247101,
+        ID : 2,
         alumnoAsesorias :{
           ID : 247101,
           nombre: "Cynthia Maritza",
@@ -47,7 +78,7 @@ export class ProximasTutorComponent implements OnInit {
           }
         ]
       },
-      fechaPeticion : "25/01/2023",
+      fechaPeticion : "25/01/2022",
       urgencia : false,
       materiaAsociada : {
         ID : 1,
@@ -56,17 +87,41 @@ export class ProximasTutorComponent implements OnInit {
       },
       tema : "Ciclos",
       descripcion: "No le entiendo a mi profe",
-      fechaAsesoria : "27/01/2023 17:00",
-      sitio : "",
-      modalidad : "",
+      fechaAsesoria : "22/01/2023 16:00",
+      sitio : "BIBLIOTECA",
+      modalidad : "Presencial",
       tutoresNoDisponibles : []
     }
+    //inicializamos solicitud detalle con cualquier cosa
+    this.solicitudDetalle = solicitud1;
+    this.solicitudes.push(solicitud1);
+  }
 
-    this.solicitudes.push(solicitud1);
-    this.solicitudes.push(solicitud1);
-    this.solicitudes.push(solicitud1);
+  mostrarSolicitud(solicitudA : solicitud) : boolean{
+
     
+    //La asesoría no es del tutor
+    if(this.tutorActual.ID != solicitudA.tutorAsesorias.ID)
+      return false;
+
+    //La solicitud no tiene fecha para llevarse acabo 
+    if(solicitudA.fechaAsesoria == "")
+      return false;
+
+    return true;
+  }
+
+  verMas( id : number):void{
+    const indice  = this.solicitudes.map(sol => sol.ID).indexOf(id);
+    this.solicitudDetalle = this.solicitudes[indice];
+    //alert("La solicitud es: "+this.solicitudDetalle.ID);
+  }
+
+  aceptar() : void{
 
   }
+
+
+
 
 }
