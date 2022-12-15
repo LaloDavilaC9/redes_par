d13574e27396451b57b45f2bf343fe98c3c2d1f6
Cynthia Teran <al226582@edu.uaa.mx>
2022-12-15 03:13:35 -0600

    Capacitor

---
+++ sistema_tutorias/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'sistema_tutorias',
+  webDir: 'dist/sistema_tutorias',
+  bundledWebRuntime: false
+};
+
+export default config;
